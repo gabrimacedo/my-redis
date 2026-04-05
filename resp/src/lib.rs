157e00cc49db7beca3731ab4848686e457a53bcf
macedo <gabrimacedo@posteo.com>
2026-04-05 16:39:31 -0300

    [refactor] split (most) grouped tests assertions into separate functions.

--- resp/src/lib.rs
+++ resp/src/lib.rs
@@ -115,119 +115,178 @@ mod tests {
     mod decoding_produces_frame {
         use super::super::*;
 
-        #[test]
-        fn simple_string() {
-            let b = b"+PONG\r\n";
-            let (f, _) = Frame::decode(b);
-            assert_eq!(f, Frame::SimpleString("PONG".to_string()));
+        mod simple_string {
+            use crate::Frame;
 
-            let b = b"+OK\r\n+PING\r\n";
-            let (f, consumed) = Frame::decode(b);
-            let (f2, _) = Frame::decode(&b[consumed..]);
-            assert_eq!(f, Frame::SimpleString("OK".to_string()));
-            assert_eq!(f2, Frame::SimpleString("PING".to_string()));
+            #[test]
+            fn single() {
+                let b = b"+PONG\r\n";
+
+                let (f, consumed) = Frame::decode(b);
+
+                assert_eq!(f, Frame::SimpleString("PONG".to_string()));
+                assert_eq!(consumed, 7);
+            }
+            #[test]
+            fn multiple() {
+                let b = b"+OK\r\n+PING\r\n";
+
+                let (f, consumed) = Frame::decode(b);
+                let (f2, _) = Frame::decode(&b[consumed..]);
+
+                assert_eq!(f, Frame::SimpleString("OK".to_string()));
+                assert_eq!(f2, Frame::SimpleString("PING".to_string()));
+            }
         }
 
         #[test]
         fn error() {
             let b = b"-ERR unknown command 'FOO'\r\n";
+
             let (f, consumed) = Frame::decode(b);
 
             assert_eq!(f, Frame::Error("ERR unknown command 'FOO'".to_string()));
             assert_eq!(consumed, 28);
         }
 
-        #[test]
-        fn integer() {
-            let b = b":42\r\n";
-            let (f, consumed) = Frame::decode(b);
+        mod integer {
+            use crate::Frame;
 
-            assert_eq!(f, Frame::Integer(42));
-            assert_eq!(consumed, 5);
+            #[test]
+            fn positive() {
+                let b = b":42069\r\n";
+
+                let (f, consumed) = Frame::decode(b);
+
+                assert_eq!(f, Frame::Integer(42069));
+                assert_eq!(consumed, 8);
+            }
+            #[test]
+            fn negative() {
+                let b = b":-1337\r\n";
+
+                let (f, consumed) = Frame::decode(b);
+
+                assert_eq!(f, Frame::Integer(-1337));
+                assert_eq!(consumed, 8);
+            }
         }
 
-        #[test]
-        fn bulk_string() {
-            let b = b"$26\r\nhello world, how are you!?\r\n";
-            let long = Frame::BulkString(b"hello world, how are you!?".to_vec());
+        mod bulk_string {
+            use crate::Frame;
 
-            let (f, consumed) = Frame::decode(b);
-            assert_eq!(f, long);
-            assert_eq!(consumed, 33);
+            #[test]
+            fn non_empty() {
+                let b = b"$26\r\nhello world, how are you!?\r\n";
+                let expected = Frame::BulkString(b"hello world, how are you!?".to_vec());
 
-            let b = b"$0\r\n\r\n";
-            let empty = Frame::BulkString(b"".to_vec());
+                let (f, consumed) = Frame::decode(b);
 
-            let (f, consumed) = Frame::decode(b);
-            assert_eq!(f, empty);
-            assert_eq!(consumed, 6);
+                assert_eq!(f, expected);
+                assert_eq!(consumed, 33);
+            }
+            #[test]
+            fn empty() {
+                let b = b"$0\r\n\r\n";
+                let expected = Frame::BulkString(b"".to_vec());
+
+                let (f, consumed) = Frame::decode(b);
+
+                assert_eq!(f, expected);
+                assert_eq!(consumed, 6);
+            }
         }
 
-        #[test]
-        fn null() {
-            let b = b"$-1\r\n";
-            let expected = Frame::Null;
+        mod null {
+            use crate::Frame;
 
-            let (f, consumed) = Frame::decode(b);
-            assert_eq!(f, expected);
-            assert_eq!(consumed, 5);
+            #[test]
+            fn from_bulk_string() {
+                let b = b"$-1\r\n";
+                let expected = Frame::Null;
+
+                let (f, consumed) = Frame::decode(b);
+
+                assert_eq!(f, expected);
+                assert_eq!(consumed, 5);
+            }
+            #[test]
+            fn from_array() {
+                let b = b"*-1\r\n";
+                let expected = Frame::Null;
+
+                let (f, consumed) = Frame::decode(b);
+
+                assert_eq!(f, expected);
+                assert_eq!(consumed, 5);
+            }
         }
 
-        #[test]
-        fn array() {
-            let b = b"*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$5\r\nhello\r\n+OK\r\n";
-            let expected = Frame::Array(Vec::from([
-                Frame::BulkString(b"SET".to_vec()),
-                Frame::BulkString(b"mykey".to_vec()),
-                Frame::BulkString(b"hello".to_vec()),
-            ]));
+        mod array {
+            use crate::Frame;
 
-            let (f, consumed) = Frame::decode(b);
-            assert_eq!(f, expected);
-            assert_eq!(consumed, 35);
+            #[test]
+            fn empty() {
+                let b = b"*0\r\n";
+                let expected = Frame::Array(Vec::new());
 
-            // empty
-            let b = b"*0\r\n";
-            let empty = Frame::Array(Vec::new());
+                let (f, consumed) = Frame::decode(b);
 
-            let (f, consumed) = Frame::decode(b);
-            assert_eq!(f, empty);
-            assert_eq!(consumed, 4);
+                assert_eq!(f, expected);
+                assert_eq!(consumed, 4);
+            }
+            #[test]
+            fn simple() {
+                let b = b"*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$5\r\nhello\r\n+OK\r\n";
+                let expected = Frame::Array(Vec::from([
+                    Frame::BulkString(b"SET".to_vec()),
+                    Frame::BulkString(b"mykey".to_vec()),
+                    Frame::BulkString(b"hello".to_vec()),
+                ]));
 
-            // negative (null)
-            let b = b"*-1\r\n";
-            let expected = Frame::Null;
+                let (f, consumed) = Frame::decode(b);
 
-            let (f, consumed) = Frame::decode(b);
-            assert_eq!(f, expected);
-            assert_eq!(consumed, 5);
+                assert_eq!(f, expected);
+                assert_eq!(consumed, 35);
+            }
+            #[test]
+            fn nested() {
+                // Input bytes: an array containing [integer 1, array [simple string "OK", integer 2]]
+                let b = b"*2\r\n:1\r\n*2\r\n+OK\r\n:2\r\n";
+                let expected = Frame::Array(vec![
+                    Frame::Integer(1),
+                    Frame::Array(vec![
+                        Frame::SimpleString("OK".to_string()),
+                        Frame::Integer(2),
+                    ]),
+                ]);
 
-            // nested
-            // Input bytes: an array containing [integer 1, array [simple string "OK", integer 2]]
-            let b = b"*2\r\n:1\r\n*2\r\n+OK\r\n:2\r\n";
+                let (f, consumed) = Frame::decode(b);
 
-            let expected = Frame::Array(vec![
-                Frame::Integer(1),
-                Frame::Array(vec![
-                    Frame::SimpleString("OK".to_string()),
-                    Frame::Integer(2),
-                ]),
-            ]);
-
-            let (f, consumed) = Frame::decode(b);
-            assert_eq!(f, expected);
-            assert_eq!(consumed, 21);
+                assert_eq!(f, expected);
+                assert_eq!(consumed, 21);
+            }
         }
     }
 
     mod encoding_produces_prefix_and_crlf {
-        use super::super::*;
+        use crate::Frame;
 
-        #[test]
-        fn integer() {
-            assert_eq!(Frame::Integer(42).encode(), b":42\r\n");
-            assert_eq!(Frame::Integer(-1).encode(), b":-1\r\n");
-            assert_eq!(Frame::Integer(0).encode(), b":0\r\n");
+        mod integer {
+            use crate::Frame;
+
+            #[test]
+            fn positive() {
+                assert_eq!(Frame::Integer(42).encode(), b":42\r\n");
+            }
+            #[test]
+            fn negative() {
+                assert_eq!(Frame::Integer(-1).encode(), b":-1\r\n");
+            }
+            #[test]
+            fn zero() {
+                assert_eq!(Frame::Integer(0).encode(), b":0\r\n");
+            }
         }
 
         #[test]
@@ -239,39 +298,68 @@ mod tests {
             assert_eq!(longer.encode(), b"+PONG\r\n");
         }
 
-        #[test]
-        fn bulk_string() {
-            // arrange
-            let short = Frame::BulkString("hello".as_bytes().to_vec());
-            let long = Frame::BulkString("hello world, how are you!?".as_bytes().to_vec());
-            let empty = Frame::BulkString("".as_bytes().to_vec());
+        mod bulk_string {
+            use crate::Frame;
 
-            assert_eq!(short.encode(), b"$5\r\nhello\r\n");
-            assert_eq!(long.encode(), b"$26\r\nhello world, how are you!?\r\n");
-            assert_eq!(empty.encode(), b"$0\r\n\r\n");
+            #[test]
+            fn empty() {
+                let empty = Frame::BulkString("".as_bytes().to_vec());
+
+                assert_eq!(empty.encode(), b"$0\r\n\r\n");
+            }
+            #[test]
+            fn non_empty() {
+                let short = Frame::BulkString("hello".as_bytes().to_vec());
+                let long = Frame::BulkString("hello world, how are you!?".as_bytes().to_vec());
+
+                assert_eq!(short.encode(), b"$5\r\nhello\r\n");
+                assert_eq!(long.encode(), b"$26\r\nhello world, how are you!?\r\n");
+            }
         }
 
-        #[test]
-        fn array() {
-            let arr = Frame::Array(Vec::from([
-                Frame::BulkString(b"SET".to_vec()),
-                Frame::BulkString(b"mykey".to_vec()),
-                Frame::BulkString(b"hello".to_vec()),
-            ]))
-            .encode();
+        mod array {
+            use crate::Frame;
 
-            let simp_arr = Frame::Array(Vec::from([
-                Frame::SimpleString("OK".to_string()),
-                Frame::SimpleString("OK".to_string()),
-                Frame::BulkString(b"1".to_vec()),
-            ]))
-            .encode();
+            #[test]
+            fn empty() {
+                let empty = Frame::Array(Vec::new()).encode();
 
-            let empty = Frame::Array(Vec::new()).encode();
+                assert_eq!(empty, b"*0\r\n");
+            }
+            #[test]
+            fn non_empty() {
+                let b = Frame::Array(Vec::from([
+                    Frame::BulkString(b"SET".to_vec()),
+                    Frame::BulkString(b"mykey".to_vec()),
+                    Frame::BulkString(b"hello".to_vec()),
+                ]))
+                .encode();
 
-            assert_eq!(arr, b"*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$5\r\nhello\r\n");
-            assert_eq!(simp_arr, b"*3\r\n+OK\r\n+OK\r\n$1\r\n1\r\n");
-            assert_eq!(empty, b"*0\r\n");
+                let b2 = Frame::Array(Vec::from([
+                    Frame::SimpleString("OK".to_string()),
+                    Frame::SimpleString("OK".to_string()),
+                    Frame::BulkString(b"1".to_vec()),
+                ]))
+                .encode();
+
+                assert_eq!(b, b"*3\r\n$3\r\nSET\r\n$5\r\nmykey\r\n$5\r\nhello\r\n");
+                assert_eq!(b2, b"*3\r\n+OK\r\n+OK\r\n$1\r\n1\r\n");
+            }
+            #[test]
+            fn nested() {
+                let b = Frame::Array(vec![
+                    Frame::Integer(1),
+                    Frame::Array(vec![
+                        Frame::SimpleString("OK".to_string()),
+                        Frame::Integer(2),
+                    ]),
+                ])
+                .encode();
+
+                let expected = b"*2\r\n:1\r\n*2\r\n+OK\r\n:2\r\n";
+
+                assert_eq!(b, expected);
+            }
         }
 
         #[test]
@@ -281,15 +369,14 @@ mod tests {
 
         #[test]
         fn error() {
-            assert_eq!(
-                Frame::Error("ERR unknow command 'cmd'".to_string()).encode(),
-                b"-ERR unknow command 'cmd'\r\n"
+            let b = Frame::Error("ERR unknow command 'cmd'".to_string());
+            let b2 = Frame::Error(
+                "WRONGTYPE Operation against a key holding the wrong kind of value".to_string(),
             );
+
+            assert_eq!(b.encode(), b"-ERR unknow command 'cmd'\r\n");
             assert_eq!(
-                Frame::Error(
-                    "WRONGTYPE Operation against a key holding the wrong kind of value".to_string()
-                )
-                .encode(),
+                b2.encode(),
                 b"-WRONGTYPE Operation against a key holding the wrong kind of value\r\n"
             )
         }
