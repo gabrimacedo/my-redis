e11efcee071388afa612a2b4b372631fa8a1743e
macedo <gabrimacedo@posteo.com>
2026-04-21 19:19:03 -0300

    [feat] Server: Connection struct with buffered reads, cursor compaction, error handling

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashMap,
+    io,
     time::{Duration, Instant},
 };
 
@@ -244,8 +245,6 @@ impl Command {
     }
 }
 
-pub fn run() {}
-
 type CommandRequest = (Command, oneshot::Sender<Frame>);
 
 pub async fn start_server(listener: TcpListener) {
@@ -269,25 +268,87 @@ pub async fn start_server(listener: TcpListener) {
     }
 }
 
-async fn handle_client(mut socket: TcpStream, tx: Sender<CommandRequest>) {
+struct Connection {
+    stream: TcpStream,
+    buffer: Vec<u8>,
+    cursor: usize,
+}
+
+impl Connection {
+    fn new(stream: TcpStream) -> Self {
+        Self {
+            buffer: Vec::new(),
+            cursor: 0,
+            stream,
+        }
+    }
+
+    async fn read_frame(&mut self) -> io::Result<Frame> {
+        // TODO: investigate better memory efficiency for internal buffer
+        loop {
+            // read frame from internal buffer
+            let (in_frame, consumed) = Frame::decode(&self.buffer[self.cursor..]);
+
+            if matches!(in_frame, Frame::Incomplete) {
+                // grow the buffer to read into new space
+                let len = self.buffer.len();
+                self.buffer.resize(len + 128, 0);
+
+                // read from stream & append to internal buffer
+                let n = self.stream.read(&mut self.buffer[len..]).await?;
+                if n == 0 {
+                    return Err(io::Error::new(
+                        io::ErrorKind::UnexpectedEof,
+                        "client disconnected",
+                    ));
+                }
+
+                // truncate to used size
+                self.buffer.truncate(len + n);
+                continue;
+            }
+
+            self.cursor += consumed;
+            if self.cursor > self.buffer.len() / 2 {
+                self.buffer.drain(..self.cursor);
+                self.cursor = 0;
+            }
+            return Ok(in_frame);
+        }
+    }
+
+    async fn write_frame(&mut self, frame: &Frame) -> io::Result<usize> {
+        self.stream.write(&frame.encode()).await
+    }
+}
+
+async fn handle_client(socket: TcpStream, tx: Sender<CommandRequest>) {
+    let mut conn = Connection::new(socket);
+
     loop {
-        // read
-        let mut buf = [0u8; 1024];
-        let nr = socket.read(&mut buf).await.unwrap();
-        let req = buf[..nr].to_vec();
+        let frame = match conn.read_frame().await {
+            Ok(frame) => frame,
+            Err(e) => {
+                eprint!("client error: {e}");
+                return;
+            }
+        };
 
-        // decode frame TODO: handle imcomplete frame
-        let (in_frame, _) = Frame::decode(&req);
-
-        match Command::parse(in_frame) {
+        match Command::parse(frame) {
             Ok(cmd) => {
                 let (resp_tx, resp_rx) = oneshot::channel();
                 tx.send((cmd, resp_tx)).await.unwrap();
-                let resp = resp_rx.await.unwrap();
-                let _ = socket.write(&resp.encode()).await.unwrap();
+                let out_frame = resp_rx.await.unwrap();
+                if let Err(err) = conn.write_frame(&out_frame).await {
+                    eprint!("client error {err}");
+                    return;
+                }
             }
             Err(msg) => {
-                let _ = socket.write(&Frame::Error(msg).encode()).await.unwrap();
+                if let Err(err) = conn.write_frame(&Frame::Error(msg)).await {
+                    eprintln!("client error {err}");
+                    return;
+                }
             }
         };
     }
